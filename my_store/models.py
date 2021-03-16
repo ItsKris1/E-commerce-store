e921efb09c6b2979f9aa6374cf46f7f5dc8b1b21
ItsKris1 <kkangro7@gmail.com>
2021-03-16 11:58:06 +0200

    Added model field description

--- my_store/models.py
+++ my_store/models.py
@@ -4,8 +4,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=60)
-    quantity = models.IntegerField(blank=True, default='None')
+    quantity = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
