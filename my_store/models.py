5d3368a83010e3252bb143034952a006689ec11c
ItsKris1 <kkangro7@gmail.com>
2021-03-26 17:55:45 +0200

    Now cards instead of table, category as dropdown

--- my_store/models.py
+++ my_store/models.py
@@ -7,9 +7,10 @@ class Product(models.Model):
     quantity = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
     category = models.ForeignKey(
         'Category',
-        on_delete=models.CASCADE,
+        on_delete=models.DO_NOTHING,
     )
 
     def __str__(self):
--- store/settings.py
+++ store/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/products_list'
 
