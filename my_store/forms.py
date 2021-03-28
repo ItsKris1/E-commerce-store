e800820a3cb4a742700f8bddcfb1954233243213
ItsKris1 <kkangro7@gmail.com>
2021-03-28 19:28:25 +0300

    Upload a picture through creating a product, added products

--- my_store/forms.py
+++ my_store/forms.py
@@ -10,13 +10,15 @@ class ProductCreateForm(forms.ModelForm):
                   'price',
                   'description',
                   'category',
+                  'image',
                   )
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'})
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
 
